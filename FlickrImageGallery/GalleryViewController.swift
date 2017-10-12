6f0361cb23dbc6069e456295e3c241626e0a9e10
adimango <alex.dimango@gmail.com>
2017-10-12 10:38:15 +0200

    initial commit

--- FlickrImageGallery/GalleryViewController.swift
+++ FlickrImageGallery/GalleryViewController.swift
@@ -72,17 +72,6 @@ class GalleryViewController: UIViewController, GalleryViewDisplayLogic {
         alertController.addAction(alertAction)
         self.present(alertController, animated: true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
 
 extension GalleryViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
